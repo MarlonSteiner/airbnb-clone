1b97330727567ad39d3fb2448bd25310b5b24beb
Marlon Steiner <msteiner.sudo@gmail.com>
2025-11-16 05:25:23 +0100

    creating flats

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React from 'react';
+// import logo from './logo.svg';
 import './App.css';
+import Flat from './components/flat';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+      const flat = {
+        "id": 148,
+        "name": "Trendy Apt in Buttes Montmartre",
+        "imageUrl": "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat2.jpg",
+        "price": 200,
+        "priceCurrency": "EUR",
+        "lat": 48.885707,
+        "lng": 2.343543
+      };
+
+      return (
+        <div className="app">
+          <div className="main">
+            <div className="search">
+            </div>
+            <div className="flats">
+              <Flat flat={flat} />
+            </div>
+          </div>
+        </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,9 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Hello from './components/hello';
+// import Hello from './components/hello';
 // import registerServiceWorker from './registerServiceWorker';
+// const nameElement = (
+//   <div>
+//     <Hello firstName="Marlon" lastName="Steiner" />
+//     </div>
+// );
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Hello firstName="Marlon" lastName="Steiner"/>);
+root.render(<App />);
 // registerServiceWorker();
