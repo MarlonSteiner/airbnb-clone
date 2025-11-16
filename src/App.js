a45bd0b60c1ac1529ff91bedcdc9e90294b7da92
Marlon Steiner <msteiner.sudo@gmail.com>
2025-11-16 07:08:23 +0100

    integrated map and starting to set markers

--- src/App.js
+++ src/App.js
@@ -1,31 +1,50 @@
 import React from 'react';
-// import logo from './logo.svg';
+import GoogleMapReact from 'google-map-react';
 import './App.css';
 import Flat from './components/flat';
 
 class App extends React.Component {
-  render() {
-      const flat = {
-        "id": 148,
-        "name": "Trendy Apt in Buttes Montmartre",
-        "imageUrl": "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat2.jpg",
-        "price": 200,
-        "priceCurrency": "EUR",
-        "lat": 48.885707,
-        "lng": 2.343543
-      };
+  constructor(props) {
+    super(props);
+    this.state = {
+      flats: []
+    };
+  }
 
-      return (
-        <div className="app">
-          <div className="main">
-            <div className="search">
-            </div>
-            <div className="flats">
-              <Flat flat={flat} />
-            </div>
+  componentDidMount() {
+    console.log("Did mount");
+    fetch("https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json")
+    .then(response => response.json())
+    .then((data) => {
+      this.setState({ flats: data });
+    });
+  }
+
+  render() {
+    const center = {
+      lat: 48.8566,
+      lng: 2.3522
+    };
+    return (
+      <div className="app">
+        <div className="main">
+          <div className="search">
+          </div>
+          <div className="flats">
+            {this.state.flats.map((flat) => {
+              return <Flat flat={flat} />
+            })}
           </div>
         </div>
-    );
+        <div className="map">
+          <GoogleMapReact
+            center={ center }
+            zoom={14}
+          >
+          </GoogleMapReact>
+        </div>
+      </div>
+  );
   }
 }
 
